a0adc5938fb0e26d874b802a50e5ca5f9048babb
Iseri Katashi <iserikatashi@gmail.com>
2025-12-30 00:51:03 +0600

    Add main CSS styles for game board and pieces
    - Implemented global styles to reset margin and padding, and set box-sizing.
    - Created styles for the display area with a background image.
    - Designed the game board with a grid layout and background image.
    - Defined styles for cells and pieces, ensuring proper sizing and positioning.
    - Added highlight effect for selected pieces using pseudo-elements.

---
+++ script/main.js
@@ -0,0 +1,6 @@
+import {initializeBoard} from "./ui/renderBoard.js";
+
+// Initialize the chessboard when the window loads
+window.onload = function() {
+  initializeBoard();
+}
---
+++ script/ui/renderBoard.js
@@ -0,0 +1,43 @@
+// This function initializes the chessboard by creating cells, assigning IDs, placing pieces, and setting up click handlers for piece movement rendering.
+
+import { initial } from "../engine/initial.js";
+export function initializeBoard() {
+  // Create board cells
+  const board = document.getElementById("board");
+  const rowsLetters = ["a", "b", "c", "d", "e", "f", "g", "h"];
+
+  for (let row = 0; row < 64; row += 8) {
+    for (let col = 0; col < 8; col++) {
+      const cell = document.createElement("div");
+      cell.classList.add("cell");
+      cell.id = rowsLetters[col] + (8 - row / 8);
+      board.appendChild(cell);
+    }
+  }
+
+  // Place pieces on the board
+  const cells = initial();
+  console.log(cells)
+
+  const pieceImgSrc = {
+    br: "script/imgs/pieces/br.png",
+    bn: "script/imgs/pieces/bn.png",
+    bb: "script/imgs/pieces/bb.png",
+    bq: "script/imgs/pieces/bq.png",
+    bk: "script/imgs/pieces/bk.png",
+    bp: "script/imgs/pieces/bp.png",
+    wr: "script/imgs/pieces/wr.png",
+    wn: "script/imgs/pieces/wn.png",
+    wb: "script/imgs/pieces/wb.png",
+    wq: "script/imgs/pieces/wq.png",
+    wk: "script/imgs/pieces/wk.png",
+    wp: "script/imgs/pieces/wp.png",
+  };
+
+  for (const cell in cells) {
+    if (cells[cell]) {
+      const cellElement = document.getElementById(cell);
+      cellElement.innerHTML = `<img class="piece" src="${pieceImgSrc[cells[cell]]}" alt="${cells[cell]}">`
+    }
+  }
+}
